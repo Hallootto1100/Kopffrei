a6ed2c2466939a1c7fd60d1461fd697db94e19ef
Armin Kessler <armin.kessler@me.com>
2025-08-08 09:18:13 +0200

    refactor: Remove unused navigation JavaScript and clean up CSS styles

--- nav.js
+++
@@ -1,6 +0,0 @@
-const navToggle = document.querySelector('.nav-toggle');
-const nav = document.querySelector('.main-nav ul');
-
-navToggle.addEventListener('click', () => {
-    nav.classList.toggle('nav-visible');
-});
